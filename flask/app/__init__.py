47023605ace88112b2a2e6c324206c92504322eb
josephzxy <josephchuh7@gmail.com>
2018-07-09 15:38:21 +0800

    Finished user login page

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
+
+# 写在这里的是不是包全局变量？
+# 只要from app import 就可以使用了

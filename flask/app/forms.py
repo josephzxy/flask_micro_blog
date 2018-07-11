96c29596b744cf57c2133cd1d30a41a9612b49a8
josephzxy <josephchuh7@gmail.com>
2018-07-11 17:46:24 +0800

    Fixed username conflict bug when user modifies their profile.

--- flask/app/forms.py
+++ flask/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(message='Pls input username')])
@@ -12,3 +13,18 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(message='Pls input username')])
     about_me = TextAreaField('About', validators=[Length(min=0, max=140)])
     submit = SubmitField('submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            # search to see if the usernaem already exists in database
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
+            #     self.username.errors.append('This username is already in use. Please choose another one.')
+            #     return False
+            # return True

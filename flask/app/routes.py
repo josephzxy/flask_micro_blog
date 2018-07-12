d8fdd769996752c92655d17e69951c6274fcc450
josephzxy <josephchuh7@gmail.com>
2018-07-12 21:53:29 +0800

    Finished Page Navigation

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -20,8 +20,14 @@ def index():
         return redirect(url_for('index'))
 
     page = request.args.get('page', 1, type=int)
-    posts = current_user.get_followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False).items
-    return render_template('index.html', title='Home Page', form=form, posts=posts)
+    posts = current_user.get_followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template(
+        'index.html',
+         title='Home', form=form, posts=posts.items,
+         next_url=next_url, prev_url=prev_url
+    )
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -154,5 +160,9 @@ def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'], False
-    ).items
-    return render_template('index.html', title='Explore', posts=posts)
+    )
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    return render_template(
+        'index.html', title='Explore', posts=posts.items,
+        next_url=next_url, prev_url=prev_url)

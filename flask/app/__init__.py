3e2fc5bae3ce661257e21cff9d3781201eda503b
josephzxy <josephchuh7@gmail.com>
2018-07-13 18:08:32 +0800

    Basic refactor done.

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -4,14 +4,36 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = 'Please log in to access this page.'
+# mail = Mail()
+# bootstrap = Bootstrap
+# moment = Momnet()
+# babel = Babel()
 
-from app import routes, models
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-# 写在这里的是不是包全局变量？
-# 只要from app import 就可以使用了
+    db.init_app(app)
+    migrate.init_app(app)
+    login.init_app(app)
+
+    # mail.init_app(app)
+    # bootstrap.init_app(app)
+    # moment.init_app(app)
+    # babel.init_app(app)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')# all url in auth will be like http://localhost:5000/auth/...
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
